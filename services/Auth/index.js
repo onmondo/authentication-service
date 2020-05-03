836fc30dd841963405ef6af5f674a82c746927e5
John Raymond A. Blando <zjablando@917ventures.com-at-967553676970>
2020-05-03 16:49:45 +0800

    improve service performance

--- services/Auth/index.js
+++ services/Auth/index.js
@@ -1,50 +1,65 @@
+// process.env.UV_THREADPOOL_SIZE = 4;
 require('module-alias/register');
-const express = require('express');
-const morgan = require('morgan');
-const fs = require('fs');
-const path = require('path');
-const uuid = require('uuid');
+const cluster = require('cluster');
 
-// const errorHandler = require('../../common/errorHandler');
-const errorHandler = require('@common/errorHandler');
-const ServiceError = require('@utils/ServiceError');
-const logDirectory = process.env.LOGS_DIR;
+console.log('isMaster:', cluster.isMaster);
 
-const app = express();
-app.use(express.json());
-
-const accessLogStream = fs.createWriteStream(
-  path.join(`${logDirectory}`, `app-917-auth-service.log.${Date.now()}`),
-  {flags: 'a'}
-);
-
-app.use(morgan(process.env.LOGS_FORMAT, {stream: accessLogStream}));
-
-app.use(morgan(process.env.LOGS_FORMAT))
-
-const routes = require('./router');
-app.use('/api/v1/auth', routes);
-
-// operational error handler for non-existing route
-app.all('*', (req, _res, next) => {
-  req.id = uuid.v4();
-  morgan.token('id', () => req.id);
-
-  const errorMessage = `Cannot find ${req.originalUrl} on this server!`;
-  morgan.token('process', () => errorMessage);
-
-  const err = new ServiceError(
-    errorMessage,
-    `${process.env.PREFIX_ERROR_CODE_ERROR}-APP404`,
-    404,
+// Is the file being executed in master mode?
+if (cluster.isMaster) {
+  // if so, create a 4 cloned version of this server and listen request
+  cluster.fork();
+  cluster.fork();
+  cluster.fork();
+  cluster.fork();
+} else {
+  // if its not, the file behave as a normal express server
+  const express = require('express');
+  const morgan = require('morgan');
+  const fs = require('fs');
+  const path = require('path');
+  const uuid = require('uuid');
+  
+  // const errorHandler = require('../../common/errorHandler');
+  const errorHandler = require('@common/errorHandler');
+  const ServiceError = require('@utils/ServiceError');
+  const logDirectory = process.env.LOGS_DIR;
+  
+  const app = express();
+  app.use(express.json());
+  
+  const accessLogStream = fs.createWriteStream(
+    path.join(`${logDirectory}`, `app-917-auth-service.log.${Date.now()}`),
+    {flags: 'a'}
   );
-  next(err);
-});
-
-app.use(errorHandler);
-
-const port = process.env.PORT
-
-app.listen(port, () => {
-  console.log(`Auth Service running at port ${port}`);
-});
+  
+  app.use(morgan(process.env.LOGS_FORMAT, {stream: accessLogStream}));
+  
+  app.use(morgan(process.env.LOGS_FORMAT))
+  
+  const routes = require('./router');
+  app.use('/api/v1/auth', routes);
+  
+  // operational error handler for non-existing route
+  app.all('*', (req, _res, next) => {
+    req.id = uuid.v4();
+    morgan.token('id', () => req.id);
+  
+    const errorMessage = `Cannot find ${req.originalUrl} on this server!`;
+    morgan.token('process', () => errorMessage);
+  
+    const err = new ServiceError(
+      errorMessage,
+      `${process.env.PREFIX_ERROR_CODE_ERROR}-APP404`,
+      404,
+    );
+    next(err);
+  });
+  
+  app.use(errorHandler);
+  
+  const port = process.env.PORT
+  
+  app.listen(port, () => {
+    console.log(`Auth Service running at port ${port}`);
+  });
+}
